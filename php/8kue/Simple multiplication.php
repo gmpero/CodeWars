cf2cf51213c6c3c2d19ca0c86fcd2215993bc691
gmpero <rostislavpen@gmail.com>
2023-03-17 23:25:35 +0300

    up

---
+++ php/8kue/Simple multiplication.php
@@ -0,0 +1,16 @@
+<?php
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// function simpleMultiplication($number) {
+//     if($number%2==0)
+//         return $number*8;
+//     else
+//         return $number*8;
+//   }
+
+function simpleMultiplication($number) {
+    return $number%2==0 ? $number * 8 : $number * 9;
+}
+
+echo simpleMultiplication(2);
